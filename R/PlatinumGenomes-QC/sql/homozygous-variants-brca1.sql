873d83c004238ae83faf41b68f279dc12606964d
Nicole Deflaux <nicole.deflaux@gmail.com>
2014-12-18 15:20:59 -0800

    Updated codelab to make use of new VCF table.
    Also incorporated chi-squared logic from @gregmcinnes to second version of HWE query.

--- R/PlatinumGenomes-QC/sql/homozygous-variants-brca1.sql
+++ R/PlatinumGenomes-QC/sql/homozygous-variants-brca1.sql
@@ -1,27 +1,49 @@
 # Individual Homozygosity
 SELECT
-  call.call_set_name AS INDV,
-  SUM(first_allele = second_allele) AS O_HOM,
-  COUNT(call.call_set_name) AS N_SITES,
+  INDV,
+  O_HOM,
+  ROUND(E_HOM, 2) as E_HOM,
+  N_SITES,
+  ROUND((O_HOM - E_HOM) / (N_SITES - E_HOM), 5) AS F
 FROM (
   SELECT
-    call.call_set_name,
-    NTH(1,
-      call.genotype) WITHIN call AS first_allele,
-    NTH(2,
-      call.genotype) WITHIN call AS second_allele,
-    COUNT(call.genotype) WITHIN call AS ploidy
-  FROM
-    [_THE_TABLE_]
-  WHERE
-    reference_name = 'chr17'
-    AND start BETWEEN 41196311
-    AND 41277499
-  OMIT RECORD IF EVERY(alternate_bases IS NULL)
-  HAVING
-    ploidy = 2
+    call.call_set_name AS INDV,
+    SUM(first_allele = second_allele) AS O_HOM,
+    SUM(1.0 - (2.0 * freq * (1.0 - freq) * (called_allele_count / (called_allele_count - 1.0)))) AS E_HOM,
+    COUNT(call.call_set_name) AS N_SITES,
+  FROM (
+    SELECT
+      reference_name,
+      start,
+      reference_bases,
+      GROUP_CONCAT(alternate_bases) WITHIN RECORD AS alternate_bases,
+      call.call_set_name,
+      NTH(1,
+        call.genotype) WITHIN call AS first_allele,
+      NTH(2,
+        call.genotype) WITHIN call AS second_allele,
+      COUNT(alternate_bases) WITHIN RECORD AS num_alts,
+      SUM(call.genotype >= 0) WITHIN RECORD AS called_allele_count,
+      IF((SUM(1 = call.genotype) > 0),
+        SUM(call.genotype = 1)/SUM(call.genotype >= 0),
+        -1)  WITHIN RECORD AS freq
+    FROM
+      [_THE_EXPANDED_TABLE_]
+    WHERE
+      reference_name = 'chr17'
+      AND start BETWEEN 41196311
+      AND 41277499
+    OMIT
+      call IF SOME(call.genotype < 0)
+      OR (2 > COUNT(call.genotype))
+    HAVING
+      # Skip 1/2 genotypes _and non-SNP variants
+      num_alts = 1
+      AND reference_bases IN ('A','C','G','T')
+      AND alternate_bases IN ('A','C','G','T')
+      )
+  GROUP BY
+    INDV
     )
-GROUP BY
-  INDV
 ORDER BY
   INDV
