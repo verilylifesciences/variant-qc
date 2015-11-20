6d85a7e4d9452a042bc0458fb7b719177205aa4d
Nicole Deflaux <deflaux@google.com>
2015-11-20 08:04:00 -0800

    Update table names.

--- R/PlatinumGenomes-QC/rHelpers/pgpDataset.R
+++ R/PlatinumGenomes-QC/rHelpers/pgpDataset.R
@@ -16,42 +16,44 @@ require(RCurl)
 require(dplyr)
 
 # These tables contain the Complete Genomics PGP genomes and one variants-only Illumina genome.
-queryReplacements <- list("_GENOME_CALL_TABLE_"="google.com:biggene:pgp_20150205.variants",
-                          "_MULTISAMPLE_VARIANT_TABLE_"="google.com:biggene:pgp_20150205.expanded_variants")
+queryReplacements <- list("_GENOME_CALL_TABLE_"="google.com:biggene:pgp_20150205.genome_calls",
+                          "_MULTISAMPLE_VARIANT_TABLE_"="google.com:biggene:pgp_20150205.multisample_variants",
+                          "DP"="totalReadCount" # for Ti/Tv by depth
+                          )
 
 sampleData <- read.csv(textConnection(getURL("https://my.pgp-hms.org/google_surveys/1/download")))
-sampleInfo <- select(sampleData, call_call_set_name=Participant, gender=Sex.Gender)
+sampleInfo <- select(sampleData, call_call_set_name=Participant, sex=Sex.Gender)
 
 # Google Genomics variant set id for dataset 'pgp_20150205'
 variantSetId <- 9170389916365079788
 
 # Show only a subset of PGP ids in the Identity By State results to keep the
 # heatmap to a reasonable size.
-sampleIds <- c("hu03E3D2",
-               "hu03E3D2-lfr",
-               "hu040C0A",
-               "hu0486D6",
-               "hu0486D6-ilm",
-               "hu0486D6-lfr",
+sampleIds <- c("hu040C0A",
                "hu04DF3C",
                "hu04F220",
                "hu050E9C",
                "hu05FD49",
-               "hu085B6D",
-               "hu085B6D-lfr",
-               "hu24A473",
-               "hu24A473-lfr",
                "hu34D5B9-1",
                "hu34D5B9-2",
-               "hu6A01AF",
-               "hu6A01AF-lfr",
-               "huC1F1FB",
-               "huC1F1FB-lfr",
-               "huD2B804",
-               "huD2B804-lfr")
-ibs <- read.table("http://storage.googleapis.com/genomics-public-data/personal-genome-project/other/personal-genome-project-ibs.tsv",
-                  col.names=c("sample1", "sample2", "ibsScore", "similar", "observed"))
-ibs <- filter(ibs, sample1 %in% sampleIds & sample2 %in% sampleIds)
+               "hu089792",
+               "huA49E22",
+               "huFFAD87",
+               "huC14AE1",
+               "hu627574",
+               "hu5FA322",
+               "huE58004",
+               "hu2843C9",
+               "hu7B594C",
+               "hu67EBB3",
+               "hu7852C5",
+               "hu2D6140",
+               "huA02824",
+               "hu82E689",
+               "huD52556")
+# TODO: IBS
+ibs <- data.frame(col.names=c("sample1", "sample2", "ibsScore", "similar", "observed"))
+#ibs <- filter(ibs, sample1 %in% sampleIds & sample2 %in% sampleIds)
 
 # TODO: 2-way PCA against 1,000 Genomes
 pca <- data.frame(col.names=c("call_call_set_name", "PC1", "PC2", "count"))
